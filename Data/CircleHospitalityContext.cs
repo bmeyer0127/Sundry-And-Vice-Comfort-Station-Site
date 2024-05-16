9dae3d38f9ae6466ee12a17fc7b1abaf25428bb2
bmeyer0127 <bmeyer0127@gmail.com>
2024-05-16 10:08:52 -0400

    Configuring connection string for Sql Server

--- Data/CircleHospitalityContext.cs
+++ Data/CircleHospitalityContext.cs
@@ -6,15 +6,17 @@ public class CircleHospitalityContext : DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
         optionsBuilder.UseSqlServer(
-            @"Server=(local);Database=CircleHospitality;Trusted_Connection=True");
+            @"Server=(localdb)\\mssqllocaldb;Database=CircleHospitalityDb;Trusted_Connection=True;MultipleActiveResultSets=true"
+);
+
 
     public DbSet<Location> Locations { get; set; }
-  public DbSet<LocationMenu> LocationMenus { get; set; }
-  public DbSet<MenuItem> MenuItems { get; set; }
-  public DbSet<ItemType> ItemTypes { get; set; }
-  public DbSet<ItemIngredient> ItemIngredients { get; set; }
-  public DbSet<Ingredient> Ingredients { get; set; }
-  public DbSet<Measurement> Measurements { get; set; }
-  public DbSet<HoursOpen> HoursOpens { get; set; }
-  public DbSet<Gallery> Galleries { get; set; }
+    public DbSet<LocationMenu> LocationMenus { get; set; }
+    public DbSet<MenuItem> MenuItems { get; set; }
+    public DbSet<ItemType> ItemTypes { get; set; }
+    public DbSet<ItemIngredient> ItemIngredients { get; set; }
+    public DbSet<Ingredient> Ingredients { get; set; }
+    public DbSet<Measurement> Measurements { get; set; }
+    public DbSet<HoursOpen> HoursOpens { get; set; }
+    public DbSet<Gallery> Galleries { get; set; }
 }
