e285e7d79f322c8481f2996044ec6e8b86bd631c
Brett Meyer <bmeyer0127@gmail.com>
2024-09-05 19:26:19 -0400

    Currently trying to add new migration to Context

--- Data/LocationMenu.cs
+++ Data/LocationMenu.cs
@@ -9,9 +9,6 @@ public class LocationMenu
     public int MenuItemID { get; set; }
     [Required]
     public int Price { get; set; }
-    public bool HasEgg { get; set; }
-    [Required]
-    public bool HasDairy { get; set; }
 
     public virtual MenuItem? MenuItem { get; set; }
     public virtual Location? Location { get; set; }
